51c783a4b9740d45a5af6ea597a666d5e85623dc
FlyAeroMan <93842887+FlyAeroMan@users.noreply.github.com>
2023-01-18 09:25:25 -0600

    Added Code
    added main function written the night before, outlines the vision for the project and sets up the structure thereof. I discovered VSPAero's ability to calculate all the static stability and control derivatives last night, so now this code is possible using methods from Airplane Flight Dynamics and automatic Flight Controls: Part I by Jan Roskam.

---
+++ src/DynamicResponse.m
@@ -0,0 +1,70 @@
+% DynamicResponse - Intended for use with VSPAero Stability Analysis,
+% calculates the dynamic response of the aircraft given the required
+% non-dimensional static derivatives and control derivatives. User selects
+% a .stab file from VSPAero, and this code does the rest. See README for
+% more details.
+
+% Copyright (C) 2023  Kale Macormic
+%
+%     This program is free software: you can redistribute it and/or modify
+%     it under the terms of the GNU General Public License as published by
+%     the Free Software Foundation, either version 3 of the License, or
+%     (at your option) any later version.
+% 
+%     This program is distributed in the hope that it will be useful,
+%     but WITHOUT ANY WARRANTY; without even the implied warranty of
+%     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%     GNU General Public License for more details.
+% 
+%     You should have received a copy of the GNU General Public License
+%     along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+% January 17, 2023 10:56:12 PM CST -- Init Version (Kale Macormic)
+% [next version here]
+
+% NOTICE: This source code is Copyright (C) 2023  Kale Macormic and is
+% intended for small RC aircraft use only. If you obtain this
+% document in error destroy it immediately.
+
+% initialize environment
+clear all
+close all
+clc
+
+% Read in the .stab file the user selects & ask for inertia matrix
+allData = ReadStabFile;
+
+% Longitudinal Modes-------------------------------------------------------
+
+% Calculate Characteristic Equation Roots
+longRoots = LongitudinalRoots(allData);
+
+% Calculate Phugoid Damping Ratio and Frequency
+phugoidResults = PhugoidCalc(longRoots);
+
+% Calculate Short Period Damping Ratio and Frequency
+shortPeriodResults = ShortPeriodCalc(longRoots);
+
+% Lateral Modes------------------------------------------------------------
+
+% Calculate Characteristic Equation Roots
+latRoots = LateralRoots(allData);
+
+% Calculate Dutch Roll Damping Ratio and Frequency
+dutchRollResults = DutchRollCalc(latRoots);
+
+% Calculate Spiral Time to double amplitude
+spiralResults = SpiralCalc(latRoots);
+
+% Calculate Roll Mode Time Constant
+rollTimeConstant = RollTimeConstantCalc(latRoots, allData);
+
+% Calculate Roll Control Effectivness
+rollControlEffectivness = RollControlEffectivnessCalc(latRoots, allData);
+
+% Data Output--------------------------------------------------------------
+
+% Output Data to a file and/or Excel make pretty plots, etc.
+OutPutResults(allData, longRoots, phugoidResults, shortPeriodResults, ...
+    latRoots, dutchRollResults, spiralResults, rollTimeConstant, ...
+    rollControlEffectivness);
